dd8d4376fcb7d6b7751f7a890ade92a9c582a6a9
OliverChase2003 <oliverchase5408@gmail.com>
2024-11-26 13:47:40 +0800

    init

---
+++ sync.bash
@@ -0,0 +1,31 @@
+## nvim
+cp -r ~/.config/nvim ./.config/
+
+## bash
+cp ~/.bash_profile      ./.bash_profile
+cp ~/.bashrc            ./.bashrc
+cp ~/.inputrc           ./.inputrc
+
+## gitconfig
+cp ~/.gitconfig         ./.itconfig
+
+## fish
+if [ ! -d "./.config/fish"]
+    mkdir -p ./.config/fish
+fi
+
+cp ~/.config/fish/config.fish ./.config/fish/config.fish
+
+## ranger
+if [ ! -d "./.config/ranger"]
+    mkdir -p ./.config/ranger
+fi
+
+cp ~/.config/ranger/rifle.conf          ./.config/ranger/rifle.conf
+cp ~/.config/ranger/rc.conf             ./.config/ranger/rc.conf
+cp ~/.config/ranger/commands.py         ./.config/ranger/commands.py
+cp ~/.config/ranger/commands_full.py    ./.config/ranger/rcommands_full.py
+cp ~/.config/ranger/scope.sh            ./.config/ranger/scope.sh
+
+## ranger plugins
+
